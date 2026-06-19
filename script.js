8292364f13121800754c130aa512ee90e9e3664d
mtownT <eliudmucheru464@gmail.com>
2026-06-19 12:04:04 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,1 @@
-const buyButton = document.querySelector(".buy");
-const sellButton = document.querySelector(".sell");
-
-buyButton.addEventListener("click", ()=>{
-
-alert("Over trade selected");
-
-});
-
-sellButton.addEventListener("click", ()=>{
-
-alert("Under trade selected");
-
-});
+alert("JavaScript working");
